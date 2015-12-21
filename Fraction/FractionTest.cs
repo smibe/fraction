7bf9efbccd4726bfb5445a9799437e53e8382ed4
Gerhard Herbert <gerhard.herbert@avira.com>
2015-12-21 15:52:31 +0100

    First version for adding zeros

---
+++ Fraction/FractionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddTraining
+{
+    public class Fraction
+    {
+        public Fraction (int nominator)
+        {
+            Nominator = nominator;
+        }
+
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            return new Fraction(0);
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public int Nominator { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var fraction = obj as Fraction;
+            if (fraction == null)
+            {
+                return false;
+            }
+            return this.Nominator == fraction.Nominator; 
+        }
+    }
+
+    [TestClass]
+    public class FractionTest
+    {
+        [TestMethod]
+        public void AddTwoNulls()
+        {
+            Assert.AreEqual(F(0), F(0) + F(0));
+        }
+
+        public Fraction F(int nominator)
+        {
+            return new Fraction(nominator);
+        }
+    }
+}
