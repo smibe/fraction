3b59fc8bbf59e8eec5a48c5c43a3cfda3bae9ba2
Gerhard Herbert <gerhard.herbert@avira.com>
2015-12-21 15:54:51 +0100

    Added addition for integers

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,32 @@
+﻿namespace TddTraining
+{
+    public class Fraction
+    {
+        public Fraction (int nominator)
+        {
+            Nominator = nominator;
+        }
+
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            return new Fraction(first.Nominator + second.Nominator);
+        }
+
+        public override string ToString()
+        {
+            return Nominator.ToString();
+        }
+
+        public int Nominator { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var fraction = obj as Fraction;
+            if (fraction == null)
+            {
+                return false;
+            }
+            return this.Nominator == fraction.Nominator; 
+        }
+    }
+}
--- Fraction/FractionTest.cs
+++ Fraction/FractionTest.cs
@@ -4,36 +4,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TddTraining
 {
-    public class Fraction
-    {
-        public Fraction (int nominator)
-        {
-            Nominator = nominator;
-        }
-
-        public static Fraction operator +(Fraction first, Fraction second)
-        {
-            return new Fraction(0);
-        }
-
-        public override string ToString()
-        {
-            return "";
-        }
-
-        public int Nominator { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            var fraction = obj as Fraction;
-            if (fraction == null)
-            {
-                return false;
-            }
-            return this.Nominator == fraction.Nominator; 
-        }
-    }
-
     [TestClass]
     public class FractionTest
     {
@@ -41,6 +11,12 @@ namespace TddTraining
         public void AddTwoNulls()
         {
             Assert.AreEqual(F(0), F(0) + F(0));
+        }
+
+        [TestMethod]
+        public void AddTwoIntegers()
+        {
+            Assert.AreEqual(F(4), F(1) + F(3));
         }
 
         public Fraction F(int nominator)
